d27fcbdb9aea5c40500f7d002847a1115c95a628
Aditya Anand <98987504+adityaanand0001@users.noreply.github.com>
2026-01-12 00:15:36 +0530

    feat: add components/ui/avatar.js

---
+++ components/ui/avatar.js
@@ -0,0 +1,24 @@
+
+import React from "react";
+
+export const Avatar = React.forwardRef(({ className = "", ...props }, ref) => (
+  <div ref={ref} className={`relative flex h-10 w-10 shrink-0 overflow-hidden rounded-full ${className}`} {...props} />
+));
+Avatar.displayName = "Avatar";
+
+export const AvatarImage = React.forwardRef(({ className = "", ...props }, ref) => (
+  <img ref={ref} className={`aspect-square h-full w-full object-cover ${className}`} {...props} />
+));
+AvatarImage.displayName = "AvatarImage";
+
+export const AvatarFallback = React.forwardRef(({ className = "", children, ...props }, ref) => (
+  <div
+    ref={ref}
+    className={`flex h-full w-full items-center justify-center rounded-full bg-gray-200 text-gray-600 font-medium ${className}`}
+    {...props}
+  >
+    {children}
+  </div>
+));
+AvatarFallback.displayName = "AvatarFallback";
+;
